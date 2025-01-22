2121ddbedd8b5139d691d515ca70a984c294abf7
mgonline86 <mgonline86@yahoo.com>
2025-01-22 19:51:45 +0200

    UPDATE: make Whatsapp Button change position with html dir

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -6,7 +6,7 @@ export default function WhatsappButton({ phoneNumber = '555199999999' }) {
     <Button
       variant="outline"
       asChild
-      className="fixed bottom-5 right-5 h-16 w-16 rounded-full bg-[#25D366] text-white hover:bg-[#25D366]/90 hover:text-white hover:scale-105 transition-all duration-300 ease-in-out z-10"
+      className="fixed bottom-5 end-5 h-16 w-16 rounded-full bg-[#25D366] text-white hover:bg-[#25D366]/90 hover:text-white hover:scale-105 transition-all duration-300 ease-in-out z-10"
     >
       <a
         href={`https://wa.me/${phoneNumber}`}
