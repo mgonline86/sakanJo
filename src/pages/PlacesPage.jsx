7b0b3061ef13347a9ad2486aa01be3cfa3a40d63
mgonline86 <mgonline86@yahoo.com>
2025-01-24 11:15:03 +0200

    UPDATE: Change PlacesFormPage Layout and its components

--- src/pages/PlacesPage.jsx
+++ src/pages/PlacesPage.jsx
@@ -8,6 +8,9 @@ import Spinner from '@/components/ui/Spinner';
 import { useAuth } from '../../hooks/index';
 import InstallModal from '@/components/ui/InstallAppModal';
 import ProductAddPopover from '@/components/ui/AddPlace';
+import { Button } from '@/components/ui/button';
+import { Add } from '@mui/icons-material';
+import { useTranslation } from 'react-i18next';
 
 const PlacesPage = () => {
   const [places, setPlaces] = useState([]);
@@ -18,6 +21,8 @@ const PlacesPage = () => {
   const onClose = () => {
     setOpen(false);
   };
+
+  const {t} = useTranslation();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -53,28 +58,15 @@ const PlacesPage = () => {
         {/* <InstallModal text={"قم بتحميل تطبيق سكني لكي تستطيع نشر اعلاناتك "} /> */}
         {/* </Link> */}
 
-        <button
-          className="inline-flex gap-1 rounded-full bg-primary px-6 py-2 text-white"
-          onClick={() => {
-            setOpen(true);
-          }}
+        <Button
+          asChild
+          className="gap-2 rounded-full px-2 md:flex md:items-center md:px-3 max-w-sm mx-auto"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 4.5v15m7.5-7.5h-15"
-            />
-          </svg>
-          Add new place
-        </button>
+          <Link to="/account/places/new">
+            <Add />{' '}
+            <span className="hidden md:block">{t('Add new place')}</span>
+          </Link>
+        </Button>
       </div>
       <div className="mx-4 mt-4">
         {places.length > 0 &&
